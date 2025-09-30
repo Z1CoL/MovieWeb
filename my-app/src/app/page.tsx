dc6e181320988ea66fccf321ae8eb941f46cd7b9
Z1CoL <zcol4615@gmail.com>
2025-09-30 13:01:03 +0800

    card list uude dutuu hiive

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -26,6 +26,15 @@ export default function Home() {
   const plugin = React.useRef(
     Autoplay({ delay: 5000, stopOnInteraction: true })
   );
+
+  const movieImages = [
+    "/movie01.png",
+    "/movie02.png",
+    "/movie03.png",
+    "/movie04.png",
+    "/movie05.png",
+  ];
+
   return (
     <div>
       {/* Navigation Bar */}
@@ -62,22 +71,60 @@ export default function Home() {
 
       {/* Carousel */}
 
-      <Carousel
-        plugins={[plugin.current]}
-        className="w-full max-w-xs"
-        onMouseEnter={plugin.current.stop}
-        onMouseLeave={plugin.current.reset}
-      >
+      <Carousel className="mt-[30px]">
         <CarouselContent>
-          {Array.from({ length: 4 }).map((_, index) => (
+          {movieImages.map((image, index) => (
             <CarouselItem key={index}>
-              <img src="movie01.png" alt="" key={index + 1} />
+              <div className="p-1">
+                <img
+                  src={image}
+                  alt={`movie ${index + 1}`}
+                  className="w-full h-[300px] object-cover rounded-lg"
+                />
+              </div>
             </CarouselItem>
           ))}
         </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
+
+        <CarouselPrevious className="ml-[10px] left-2 top-1/2 -translate-y-1/2 bg-black/50 text-white rounded-full" />
+        <CarouselNext className="mr-[10px] right-2 top-1/2 -translate-y-1/2 bg-black/50 text-white rounded-full" />
       </Carousel>
+
+      {/* All movie list */}
+
+      <section className=" w-full flex flex-col gap-[32px] pt-[30px]">
+        {/* Movie list 1 */}
+
+        <div className="flex flex-col gap-[30px] items-center justify-center">
+          {/* Movie List Header */}
+
+          <div className=" justify-between flex w-full h-[36px] px-[40px] ">
+            <p className="w-[24px] h-[20px] size-[24px] font-semibold text-[#09090B]">
+              Upcoming
+            </p>
+            <Button className="border-none hover:bg-gray-400 bg-white text-black ">
+              See More <img src="vector.svg" alt="" />
+            </Button>
+          </div>
+
+          <div className=" flex flex-wrap gap-[32px]">
+            {/* movie list */}
+
+              <Card>
+                
+              </Card>
+
+          </div>
+        </div>
+
+        {/* Movie list 2 */}
+
+        <div className=""></div>
+
+        {/* Movie list 3 */}
+
+        <div className=""></div>
+      </section>
     </div>
   );
 }
--- my-app/src/components/ui/carousel.tsx
+++ my-app/src/components/ui/carousel.tsx
@@ -235,7 +235,7 @@ export {
   type CarouselApi,
   Carousel,
   CarouselContent,
+  CarouselNext,
   CarouselItem,
   CarouselPrevious,
-  CarouselNext,
 }
